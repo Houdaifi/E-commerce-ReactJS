cb77fa92804040281f3fa52f9bfd28cbdaa5da01
Houdaifi <houdaifaamzil@gmail.com>
2022-03-27 21:50:54 +0000

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="App bg-red-500">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className="bg-gray-50">
+      <Navbar/>
     </div>
   );
 }
